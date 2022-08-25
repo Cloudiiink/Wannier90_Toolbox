Fix bugs when counting states occuring degerate point; Add dos percentage calculation and suggest with its order; Add dos_plot_no_mag.py for DOS plot; Add GaAs example image

dis_win_suggest.py
@@ -180,6 +180,21 @@ class W90():
         mask = np.logical_and(self.eband_min <= emax, self.eband_max >= emin)
         return sum(mask)
 
+    def suggest_win_max(self, emin, nwann=None, eps=4e-3):
+        '''
+        Lower bound of dis_win_max for given froz_min and nwann
+        '''
+        nwann = nwann if nwann else self.nwann
+        mask = self.eband_min >= emin
+        idx = np.argmax(mask) + nwann - 1
+        if idx >= self.nbnds:
+            # TODO handle error case for no enough states
+            print(f'There is no enough states for {emin} with {nwann} WFs!')
+            res = int(self.emax) + 1.
+        else:
+            res = self.eband_max[idx] + eps
+        return res
+
     def suggest_froz_min(self, emax, nwann=None, eps=4e-3):
         '''
         Lower bound of froz_min for given froz_max and nwann
@@ -215,13 +230,26 @@ class W90():
         elif dN > 0:
             print('Suggest dis_froz_min & dis_froz_max as following:')
             print(f'nwann: {self.nwann}    degenercy: {self.ndeg}    Fermi: {self.efermi:12.6f}')
+
             for i in range(1, dN + 1, self.ndeg):
                 # First get froz_max for nwann = nwann_input + i, then get froz_min for nwann = nwann_input and froz_max
                 froz_max = self.suggest_froz_max(emin, nwann=self.nwann+i, eps=eps)
                 froz_min = self.suggest_froz_min(froz_max, eps=eps)
                 froz_max_list.append(froz_max)
                 froz_min_list.append(froz_min)
-            return pd.DataFrame(zip(froz_min_list, froz_max_list), columns=['dis_froz_min', 'dis_froz_max'])
+
+            # number of missing states between `emin` and lowest `dis_froz_min`
+            num_missing = self.count_states((emin, min(froz_min_list)))
+            df = pd.DataFrame(zip(froz_min_list, froz_max_list), columns=['dis_froz_min', 'dis_froz_max'])
+
+            if num_missing > 0:
+                print(f'\nWANRING: There are states between given `emin`: {emin} and lowest `dis_froz_min`: {min(froz_min_list)}. Please carefully treat the suggestion of dis_froz_min / dis_froz_max and check energy range of each bands again. This situation usually happens in no-SOC system with many denegeracy point. But we still want to give you some useful energy window information with states less than number of WFs.\n')
+                for i in range(self.nwann - num_missing + 1, self.nwann, 1):
+                    new = pd.DataFrame({"dis_froz_min" : emin,
+                                        "dis_froz_max" : self.suggest_froz_max(emin, nwann=i)}, index=[1])
+                    df = df.append(new, ignore_index=True)
+
+            return df
         else:
             return pd.DataFrame(columns=['dis_froz_min', 'dis_froz_max'])
 
@@ -284,9 +312,13 @@ if __name__ == "__main__":
         print(f'There are {w90.count_states(args.erange)} states in {args.erange}.')
     elif args.mode[0].lower() == 's': # suggest
         # suggest frozen window with given energy interval
+        print(f'`dis_froz_min` and `dis_froz_max` Table:')
         df = w90.get_dis_froz_df(args.erange, eps=4e-3)
         if len(df) > 0:
             print(df)
+        # dis_windows require energy window containing states larger than number of target WFs. This will also generate some constraint for dis_windows
+        dis_win_max = w90.suggest_win_max(args.erange[0])
+        print(f'\nLowest `dis_win_max` for {args.erange[0]}: {dis_win_max}')
 
     else:
         print(f'Unsupported mode: {args.mode}')

pre_w90_tool.py
@@ -31,20 +31,27 @@ def dos_distribute(e, dos, window):
 def gen_dos_df(dos_data_total, left, right, orb_names=['s', 'py', 'pz', 'px', 'dxy', 'dyz', 'dz2', 'dxz', 'dx2']):
     structure = dos_data_total.structure
     num_sites = structure.num_sites
+    ee = dos_data_total.energies
+
+    # TODO not fail the task but use `PROCAR` to generate a result, although it might not be accurate enough.
+    if left < ee.min() or right > ee.max():
+        raise ValueError(f'CHECK YOUR INPUT! The energies in `EIGENVAL` is ranged from {ee.min()} to {ee.max()} which not include all the energy ranged from {left} to {right} you input.')
+
     dos_df = pd.DataFrame(columns=["species", "structure_id", "orb_id", "orb_name", "key_string", "dos"])
     for i in range(num_sites):
         for j in range(len(orb_names)):
             site, orb = structure[i], Orbital(j)
             dos = dos_data_total.get_site_orbital_dos(site, orb)
-            dis = dos_distribute(dos.energies, dos.densities[Spin.up], [left, right])
+            dis = dos_distribute(ee, dos.densities[Spin.up], [left, right])
             key = site.species_string + '_' + str(i) + '_' + orb_names[j]
-            new = pd.DataFrame({"species": site.species_string,
-                                "structure_id": i,
-                                "orb_id": j,
-                                "orb_name": orb_names[j],
-                                "key_string": key,
-                                "dos": dis}, index=[1])
+            new = pd.DataFrame({"species"      : site.species_string,
+                                "structure_id" : i,
+                                "orb_id"       : j,
+                                "orb_name"     : orb_names[j],
+                                "key_string"   : key,
+                                "dos"          : dis}, index=[1])
             dos_df = dos_df.append(new, ignore_index=True)
+    dos_df['dos'] = dos_df['dos'] / max(dos_df['dos'])      # Renormalize
     return dos_df
 
 def plot_dos_dis(dos_df, pct=1, selected=set(), path='.', filename='dos_analysis.png', colors=['brown', 'orange']):
@@ -59,6 +66,7 @@ def plot_dos_dis(dos_df, pct=1, selected=set(), path='.', filename='dos_analysis
     mask = np.array(color) == colors[0]
     print(f'Plot {len(df)} orbitals with {sum(mask)} selected')
     df.plot.barh(x="key_string", y="dos", color=color, figsize=(8, 20))
+    plt.grid(axis='x')
     plt.savefig(filename, dpi=200, bbox_inches='tight', transparent=True)
 
 # 返回最终选择的 轨道数量 和 轨道列表, 以 pandas.DataFrame 的形式
@@ -302,6 +310,7 @@ if __name__ == "__main__":
         export_vasp_band(args.path)
     elif args.mode[0].lower() == 'd':   # DOS Analysis
         left, right = args.erange #-4, 8
+        left, right = (right, left) if right < left else (left, right)
         pick_rate = args.pick #0 .08
 
         vasprun = Vasprun(f"{args.path}/vasprun.xml")
@@ -312,7 +321,7 @@ if __name__ == "__main__":
         print(f"\nCalculated DOS Energy Range: {left}, {right}")
 
         if len(args.extra) == 0:
-            print(dos_df[:30])
+            print(dos_df)
             norb, simple_res_df, full_res_df = dos_analysis_df(dos_df, pick_rate=pick_rate)
             nwann = norb if args.no_soc else 2 * norb
             print(f"\nNumber of Selected Orbitals: {norb}")
@@ -333,16 +342,24 @@ if __name__ == "__main__":
             from dis_win_suggest import W90, get_efermi
 
             w90 = W90(eig=f'{args.path}/EIGENVAL',
-                    path=args.path,
-                    efermi=get_efermi(args, direct=True), 
-                    nbnds_excl=0, 
-                    nwann=nwann, 
-                    ndeg=1)
-            
+                      path=args.path,
+                      efermi=get_efermi(args, direct=True), 
+                      nbnds_excl=0, 
+                      nwann=nwann, 
+                      ndeg=1)
+
             dis_froz_df = w90.get_dis_froz_df(args.erange, eps=4e-3)
-            dis_dos = [dos_given_selected(dos_data_total, (fmin, fmax), selected) for fmin, fmax in zip(dis_froz_df['dis_froz_min'], dis_froz_df['dis_froz_max'])]
-            dis_froz_dos_df = dis_froz_df.assign(dos=dis_dos)
-            dis_froz_dos_df = dis_froz_dos_df.sort_values('dos', ascending=False)
+            dis_tdos_l, dis_pdos_l, percent_l = [], [], []
+            for fmin, fmax in zip(dis_froz_df['dis_froz_min'], dis_froz_df['dis_froz_max']):
+                dis_pdos = dos_given_selected(dos_data_total, (fmin, fmax), selected)
+                dis_tdos = dos_distribute(vasprun.tdos.energies, vasprun.tdos.densities[Spin.up], (fmin, fmax))
+                percent  = dis_pdos / dis_tdos
+                dis_tdos_l.append(dis_tdos)
+                dis_pdos_l.append(dis_pdos)
+                percent_l.append(percent)
+
+            dis_froz_dos_df = dis_froz_df.assign(pdos=dis_pdos_l, tdos=dis_tdos_l, percent=percent_l)
+            dis_froz_dos_df = dis_froz_dos_df.sort_values('percent', ascending=False)
             N = len(dis_froz_dos_df)
             print(dis_froz_dos_df)
 